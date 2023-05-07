0999f872203940a66f19ae0bab0ba46594c06026
andyphancode <andyphanpxh@gmail.com>
2023-05-07 11:35:39 -0700

    Update forms.py
    Added form for editing profile

--- forms.py
+++ forms.py
@@ -16,3 +16,12 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[Length(min=8)])
 
+class EditUserForm(FlaskForm):
+    """Edit user form."""
+
+    email = StringField('E-mail', validators=[Email()])
+    profile_image_url = StringField('Profile Image URL')
+    old_password = PasswordField('Old Password')
+    new_password = PasswordField('New Password', validators=[Length(min=8)])
+    new_password_confirm = PasswordField('Confirm New Password', validators=[Length(min=8)])
+    
